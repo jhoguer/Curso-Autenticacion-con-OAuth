d179d66a5dee5d2131a8b695f6da1de1d0c64a24
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-26 18:34:16 -0500

    Implementando cors

--- api/index.js
+++ api/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
 const { config } = require('./config');
 
 const app = express();
+
+// const corsOptions = { origin: "http://example.com"};
+// app.use(cors(corsOptions));
+
+app.use(cors())
 
 // body parser
 app.use(bodyParser.json());
@@ -14,6 +20,17 @@ app.post("/api/auth/token", (req, res) => {
   const token = jwt.sign({ sub: username, email, name }, config.authJwtSecret);
   res.json({ access_token: token });
 });
+
+app.get("/api/auth/verify", (req, res, next) => {
+  const { access_token } = req.query;
+
+  try {
+    const decoded = jwt.verify(access_token, config.authJwtSecret);
+    res.json({ message: "The acces token is valid", username: decoded.sub });
+  } catch (err) {
+    next(err)
+  }
+})
 
 const server = app.listen(5000, () => {
   console.log(`Listening http://localhost:${server.address().port}`);
