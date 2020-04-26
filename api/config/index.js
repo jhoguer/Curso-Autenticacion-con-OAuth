f6d691e057871a154b185e7b0236c01b56ca68a6
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-26 15:45:47 -0500

    Firmando un JsonWebToken JWT

---
+++ api/config/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const config = {
+  authJwtSecret: process.env.AUTH_JWT_SECRET
+};
+
+module.exports = {
+  config: config
+};
---
+++ api/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const { config } = require('./config');
+
+const app = express();
+
+// body parser
+app.use(bodyParser.json());
+
+
+app.post("/api/auth/token", (req, res) => {
+  const { email, username, name } = req.body;
+  const token = jwt.sign({ sub: username, email, name }, config.authJwtSecret);
+  res.json({ access_token: token });
+});
+
+const server = app.listen(5000, () => {
+  console.log(`Listening http://localhost:${server.address().port}`);
+})
