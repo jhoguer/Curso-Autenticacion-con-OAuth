57390922e02948e4fa67d4be2cea90628c51736e
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-26 21:36:05 -0500

    Creando servicios iniciales para Authentication Code Grant

---
+++ blog/utils/encodeBasic.js
@@ -0,0 +1,5 @@
+const encodeBasic = (username, password) => {
+  return Buffer.from(`${username}:${password}`).toString("base64");
+}
+
+module.exports = encodeBasic;
---
+++ blog/utils/generateRandomString.js
@@ -0,0 +1,14 @@
+const generateRandomString = (length) => {
+  let randomString = '';
+  const possibleChars = "ABCDEFGHIJKLMNOPQRSTVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    randomString += possibleChars.charAt(
+      Math.floor(Math.random() * possibleChars.length)
+    )
+  }
+
+  return randomString;
+}
+
+module.exports = generateRandomString;
