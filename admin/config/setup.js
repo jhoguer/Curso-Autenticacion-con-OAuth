743fe9f28d2c57b7a71a5bb7f2240e84887ac73b
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-28 16:56:02 -0500

    Implementando Universal Login con Auth0 al Admin

--- admin/config/setup.js
+++ admin/config/setup.js
@@ -1,20 +1,17 @@
 require('dotenv').config();
 
 
-const env = ["SPOTIFY_CLIENT_ID", "SPOTIFY_REDIRECT_URI"];
+const env = [
+  "SPOTIFY_CLIENT_ID",
+  "SPOTIFY_REDIRECT_URI",
+  "AUTH0_DOMAIN",
+  "AUTH0_CLIENT_ID",
+  "AUTH0_API_AUDIENCE",
+  "AUTH0_REDIRECT_URI",
+];
 
 const buildEnvConfig = (acc, cur) => {
   return { ...acc, [`process.env.${cur}`]: process.env[cur] };
 }
 
 module.exports = env.reduce(buildEnvConfig, {});
-
-// require("dotenv").config();
-
-// const env = ["SPOTIFY_CLIENT_ID", "SPOTIFY_REDIRECT_URI"];
-
-// function buildEnvConfig(acc, cur) {
-//   return { ...acc, [`process.env.${cur}`]: process.env[cur] };
-// }
-
-// module.exports = env.reduce(buildEnvConfig, {});
