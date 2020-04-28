c9acdf6dcf5065bcb1e7fe6791cdeeec4a11edbb
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-27 22:25:50 -0500

    Empezando la implementacion de Implicit Grant

---
+++ admin/config/setup.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+
+const env = ["SPOTIFY_CLIENT_ID", "SPOTIFY_REDIRECT_URI"];
+
+const buildEnvConfig = (acc, cur) => {
+  return { ...acc, [`process.env,${cur}`]: process.env[cur] };
+}
+
+module.exports = env.reduce(buildEnvConfig, {});
---
+++ admin/utils/getHashParams.js
@@ -0,0 +1,14 @@
+const getHashParams = () => {
+  const hashParams = {};
+  const r = /([^&;=]+)=?([^&;]*)/g;
+  const q = window.location.hash.substring(1);
+
+  let e;
+  while((e = r.exec(q))) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+
+  return hashParams;
+}
+
+export default getHashParams;
