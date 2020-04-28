337318c8c70f95d55355bcaffb83749d7be83c25
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-28 00:37:17 -0500

    Implementado client credentials grant

--- api/index.js
+++ api/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const request = require('request');
 const { config } = require('./config');
+
+const encodeBasic = require('./utils/encodeBasic');
 
 const app = express();
 
@@ -13,6 +16,28 @@ app.use(cors())
 
 // body parser
 app.use(bodyParser.json());
+
+const getUserPlaylists = (accessToken, userId) => {
+  if (!accessToken || !userId) {
+    return Promise.resolve(null);
+  }
+
+  const options = {
+    url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true
+  };
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        reject(error);
+      }
+
+      resolve(body);
+    });
+  });
+}
 
 
 app.post("/api/auth/token", (req, res) => {
@@ -30,7 +55,33 @@ app.get("/api/auth/verify", (req, res, next) => {
   } catch (err) {
     next(err)
   }
-})
+});
+
+app.get('/api/playlists', async (req, res, next) => {
+  const { userId } = req.query;
+
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization: `Basic ${encodeBasic(config.spotifyClientId, config.spotifyClientSecret)}`
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+
+  request.post(authOptions, async (error, response, body) => {
+    if(error || response.statusCode !== 200) {
+      next(error);
+    }
+
+    const accessToken = body.access_token;
+    const userPlaylists = await getUserPlaylists(accessToken);
+
+    res.json({ playlists: userPlaylists });
+  })
+});
 
 const server = app.listen(5000, () => {
   console.log(`Listening http://localhost:${server.address().port}`);
